fd6bbd400eba97a5e945848aa73eb16210d6e16a
Kylie <chiangkaiyun@gmail.com>
2021-12-27 22:45:55 +0800

    Refactor the objc function

--- ObjcWithSwift/ViewController.m
+++ ObjcWithSwift/ViewController.m
@@ -17,16 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+
+}
+
+- (void)objcFunction {
     _alertController = [[UIAlertController alloc]initWithNibName:nil bundle:nil];
     _alertController = [UIAlertController alertControllerWithTitle:@"This is Objc" message:@"its function" preferredStyle:UIAlertControllerStyleAlert];
     
     UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
     
     [_alertController addAction:action];
+    
+    [self presentViewController:_alertController animated:YES completion:nil];
 }
 
 - (IBAction)tappedObjcButton:(id)sender {
-    [self presentViewController:_alertController animated:YES completion:nil];
+    [self objcFunction];
 }
 
 - (IBAction)tappedSwiftButton:(id)sender {
