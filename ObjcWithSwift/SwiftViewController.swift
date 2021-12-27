77fb47eda8af762878e69d3121dcfb535c2dcb63
Kylie <chiangkaiyun@gmail.com>
2021-12-27 23:06:31 +0800

    Add the Swift page

--- ObjcWithSwift/SwiftViewController.swift
+++ ObjcWithSwift/SwiftViewController.swift
@@ -15,15 +15,8 @@ class SwiftViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func tappedLeaveButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
